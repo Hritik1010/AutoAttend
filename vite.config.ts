02b8b3cc8c3294acb05ee6ea2ad1c24a5a8344cc
ThumbStack Technologies <59014362+ThumbstackTech@users.noreply.github.com>
2025-11-10 15:49:20 +0530

    Update Vite config to enable allowedHosts and host
    Signed-off-by: ThumbStack Technologies  <59014362+ThumbstackTech@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
-export default defineConfig({
-  base: '/',
+export default defineConfig({  
   plugins: [react(), cloudflare()],
   server: {
     allowedHosts: true,
